95eb2517cd9ba7dca96958afdf4ce7b6f7ad88d1
ShrinivasaPH <134434204+ShrinivasaPH@users.noreply.github.com>
2025-08-01 14:01:44 +0530

    update

---
+++ app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+from transformers import pipeline
+
+st.set_page_config(page_title="Text Summerizer", layout="wide")
+
+st.title("Abstractive text summarizer")
+
+#load model
+@st.cache_resource
+def load_summarizer():
+    return pipeline("summarization", model="facebook/bart-large-cnn")
+
+summarizer = load_summarizer()
+
+#input box
+input_text = st.text_area("Enter the text you want to summarize:", height=300)
+
+if st.button("Summarize"):
+    if input_text:
+        with st.spinner("Summarizing..."):
+            summary = summarizer(input_text, max_length=100, min_length=30, do_sample=False)
+            st.subheader("Summary:")
+            st.success(summary[0]['summary_text'])
+    else:
+        st.warning("Please enter some text to summerize.")
---
+++ modelling.py
@@ -0,0 +1,16 @@
+from transformers import pipeline
+
+# Load summarization pipeline 
+summerizer = pipeline("summarization", model="facebook/bart-large-cnn")
+
+# Input text sample
+text = """
+India’s Chandrayaan-3 mission successfully landed near the Moon’s south pole, a region previously unexplored by other missions.
+This marks a major milestone for India's space program. Scientists expect to study the Moon’s surface for water ice and other resources.
+The mission also includes a rover to explore the terrain.
+"""
+
+#Generate the summary
+summary = summerizer(text, max_length=0, min_length=25, do_sample=False)
+
+print("Summary:\n:", summary[0]['summary_text'])
